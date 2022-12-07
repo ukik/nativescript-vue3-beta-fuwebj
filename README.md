f5671e324d8fc07781941c02e0ee153e596cdda4
ukik <muhamadduki@gmail.com>
2022-12-07 23:11:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-vue3-beta-fuwebj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-vue3-beta-knv97p)
---
+++ app/components/Details.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { ref, $navigateBack } from 'nativescript-vue';
+
+const items = ref(
+  Array(10000)
+    .fill(0)
+    .map((_, index) => `Item ${index + 1}`)
+);
+</script>
+
+<template>
+  <Page actionBarHidden="true">
+    <GridLayout rows="auto, *">
+      <Label
+        text="Go Back"
+        @tap="$navigateBack"
+        class="text-center px-4 py-10 text-2xl text-gray-900 font-bold"
+      />
+
+      <ContentView row="1" class="bg-[#65adf1] rounded-t-3xl">
+        <ListView
+          :items="items"
+          separatorColor="transparent"
+          class="bg-transparent"
+        >
+          <template #default="{ item, index }">
+            <GridLayout columns="*, auto" class="px-4">
+              <GridLayout columns="auto,*" rows="auto,auto">
+                <Image
+                  loadMode="async"
+                  rowSpan="2"
+                  col="0"
+                  src="https://art.nativescript-vue.org/NativeScript-Vue-White-Green.png"
+                  stretch="none"
+                />
+                <Label
+                  col="1"
+                  row="0"
+                  :text="item"
+                  class="text-3xl pt-3 text-white"
+                />
+                <Label
+                  col="1"
+                  row="1"
+                  :text="index"
+                  class="text-3md py-1 text-white"
+                />
+              </GridLayout>
+              <ContentView col="1" class="w-5 h-5 rounded-full bg-white" />
+            </GridLayout>
+          </template>
+        </ListView>
+      </ContentView>
+    </GridLayout>
+  </Page>
+</template>
