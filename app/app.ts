a016ebc3817c3248beead08f357f1f5a6f42c87d
ukik <muhamadduki@gmail.com>
2023-01-07 18:59:13 +0800

    2 files modified

--- app/app.ts
+++ app/app.ts
@@ -3,5 +3,39 @@ import App from './components/App.vue';
 
 import ButtonPlugin from '@nativescript-community/ui-material-button/vue';
 import CardViewPlugin from '@nativescript-community/ui-material-cardview/vue';
+import ActivityIndicatorPlugin from '@nativescript-community/ui-material-activityindicator/vue';
+import BottomNavigationPlugin from '@nativescript-community/ui-material-bottom-navigation/vue';
+import BottomNavigationBarPlugin from '@nativescript-community/ui-material-bottomnavigationbar/vue';
+import { install as installBottomSheet } from '@nativescript-community/ui-material-bottomsheet';
+import BottomSheetPlugin from '@nativescript-community/ui-material-bottomsheet/vue';
+// import { install } from "@nativescript-community/ui-material-bottomsheet";
+// install();
+// import {
+//   installMixins,
+//   themer,
+// } from '@nativescript-community/ui-material-core';
+import FloatingActionButtonPlugin from '@nativescript-community/ui-material-floatingactionbutton/vue';
+import ProgressPlugin from '@nativescript-community/ui-material-progress/vue';
+import RipplePlugin from '@nativescript-community/ui-material-ripple/vue';
+import SliderPlugin from '@nativescript-community/ui-material-slider/vue';
+import SpeedDialPlugin from '@nativescript-community/ui-material-speeddial/vue';
+import TabsPlugin from '@nativescript-community/ui-material-tabs/vue';
+import TextFieldPlugin from '@nativescript-community/ui-material-textfield/vue';
+import TextViewPlugin from '@nativescript-community/ui-material-textview/vue';
 
-createApp(App).use(ButtonPlugin).use(CardViewPlugin).start();
+createApp(App)
+  .use(ButtonPlugin)
+  .use(CardViewPlugin)
+  .use(ActivityIndicatorPlugin)
+  .use(BottomNavigationPlugin)
+  .use(BottomNavigationBarPlugin)
+  .use(BottomSheetPlugin)
+  .use(FloatingActionButtonPlugin)
+  .use(ProgressPlugin)
+  .use(RipplePlugin)
+  .use(SliderPlugin)
+  .use(SpeedDialPlugin)
+  .use(TabsPlugin)
+  .use(TextFieldPlugin)
+  .use(TextViewPlugin)
+  .start();
