b88a0505afdabfbf4bfa7afb3e257b383467413f
ukik <muhamadduki@gmail.com>
2023-01-07 18:30:36 +0800

    2 files modified, 2 added, 1 deleted

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'nativescript-vue';
 import App from './components/App.vue';
 
-createApp(App).start();
+import ButtonPlugin from '@nativescript-community/ui-material-button/vue';
+import CardViewPlugin from '@nativescript-community/ui-material-cardview/vue';
+
+createApp(App).use(ButtonPlugin).use(CardViewPlugin).start();
